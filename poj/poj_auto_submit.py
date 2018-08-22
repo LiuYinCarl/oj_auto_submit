1c2c301e37724f91a78eb21f5c11b6ce3cebecf5
LiuYinCarl <bearcarl@qq.com>
2018-08-22 15:16:00 +0800

    end with poj

--- poj/poj_auto_submit.py
+++ poj/poj_auto_submit.py
@@ -1,22 +1,30 @@
 import requests
+import json
+import time
+import os
 from lxml import etree
 import base64
+from prettytable import PrettyTable
 
 VALID_STATUS_CODES = [200, 302]
 
+
 class POJ(object):
     def __init__(self):
+        with open('poj_conf.json', 'r') as f:
+            self.conf = json.load(f)
+
         self.headers = {
             'Referer': 'http://poj.org/',
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 '
                           '(KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36',
             'Host': 'poj.org'
         }
-        self.login_url = 'http://poj.org/login'
-        self.submit_url = 'http://poj.org/submit'
-        self.response_url = 'http://poj.org/status?user_id=bearcarl'
+        self.login_url = self.conf['login_url']
+        self.submit_url = self.conf['submit_url']
+        self.response_url = self.conf['response_url'] + self.conf['post_data']['user_id1']
         self.session = requests.session()
-        self.response = None
+        self.table = PrettyTable(self.conf['table'])
 
     def decode(self, code):
         byte_string = code.encode(encoding='utf-8')
@@ -24,12 +32,7 @@ class POJ(object):
         return encode_str
 
     def login(self):
-        post_data = {
-            'user_id1': 'bearcarl',
-            'password1': 'a7873215321',
-            'B1':'login',
-            'url': '/problemlist'
-        }
+        post_data = self.conf['post_data']
         try:
             response = self.session.post(self.login_url, data=post_data, headers=self.headers)
             if response.status_code in VALID_STATUS_CODES:
@@ -37,48 +40,69 @@ class POJ(object):
         except Exception as e:
             print('login error: ', e.args)
 
-    def submit_code(self):
-        with open('E:/VSproject/TSOJ/TSOJ/TSOJ/code.cpp', 'r') as f:
+    def submit_code(self, pid):
+        with open(self.conf['file_name'], 'r') as f:
             text = f.read()
             text = self.decode(text)
-        data = {
-            'problem_id': '1000',
-            # 0 G++ 1 GCC 2 JAVA 3 PASCAL 4 C++ 5 C 6 FORTRAN
-            'language': '2',
-            'source': text,
-            'submit': 'Submit',
-            'encoded': '1'
-        }
+        data = self.conf['submit_data']
+        data['source'] = text
+        data['problem_id'] = pid
         try:
-            response = self.session.post(self.submit_url, data=data,heasers=self.headers)
+            response = self.session.post(self.submit_url, data=data, headers=self.headers)
             if response.status_code in VALID_STATUS_CODES:
                 print('submit success')
         except Exception as e:
             print('submit error: ', e.args)
 
-    def get_result(self, text):
-        html = etree.HTML(text)
-        result =
+    def table_set(self):
+        column = self.conf['table']
+        for i in column:
+            self.table.align[i] = 'l'  # is L not number 1
+        self.table.padding_width = 1
 
+    def check_result(self, data):
+        try:
+            if len(data) != 9:
+                data.insert(4, 'No Data')
+                data.insert(5, 'No Data')
+            self.table.add_row(data)
+        except Exception as e:
+            print('check result error: ', e.args)
 
-    def get_response(self):
+    def get_result(self, text, pid):
+        try:
+            html = etree.HTML(text)
+            result = html.xpath('//tr[@align = "center"]/td[3]/a[text() = %d]/../..' % pid)
+            for item in result:
+                pre_string = etree.tostring(item)
+                pre_problem = etree.HTML(pre_string)
+                valid_result = pre_problem.xpath('//text()')[:-1]
+                self.check_result(valid_result)
+        except Exception as e:
+            print('get result error: ', e.args)
+
+    def get_response(self, pid):
         try:
             response = self.session.get(self.response_url, headers=self.headers)
             if response.status_code in VALID_STATUS_CODES:
+                self.get_result(response.text, pid)
                 print('get response success')
-                self.get_result(response.text)
         except Exception as e:
             print('get response error: ', e.args)
 
-
-
-
-
-
+    def scheduling(self, pid):
+        self.login()
+        self.submit_code(pid)
+        while True:
+            self.table = PrettyTable(self.conf['table'])
+            self.table_set()
+            time.sleep(self.conf['interval_time'])
+            self.get_response(pid)
+            os.system('cls')
+            print(self.table)
 
 
 if __name__ == '__main__':
-    test = POJ()
-    # test.login()
-    test.submit_code()
-
+    auto_submit = POJ()
+    problem_id = input('enter you problem ID(such as 1002): ')
+    auto_submit.scheduling(int(problem_id))
